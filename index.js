4aa0c78f6ef66c2461cb99e29212787e72320be3
Anton M <mariadasanton27@gmail.com>
2022-02-02 00:18:53 +0100

    serveur express en commentaire

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 require('./config/dbConfig');
 const data = require('./api/data');
-const express = require('express');
+// const express = require('express');
 
-const app = express();
-const port = 8282;
+// const app = express();
+// const port = process.env.PORT || 8282;
 
 data.refreshCompaniesData();
 
-app.listen(port, () => console.log(`listening on http//:localhost:${port}`));
+// app.listen(port, () => console.log(`listening on http//:localhost:${port}`));
